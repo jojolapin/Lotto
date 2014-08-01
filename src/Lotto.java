59d40cf7220261b4926fa22f616738e8b54d82ac
Joel Ilboudo <joel.t.ilboudo@nasa.gov>
2014-08-01 17:20:07 -0400

    Applying Ctrl Shift F to java formmat the code

--- src/Lotto.java
+++ src/Lotto.java
@@ -1,16 +1,12 @@
-
 import java.io.IOException;
 
 import javax.swing.JButton;
 
-
 public class 
     Lotto
-
 {
     public static void main(String[] args) throws IOException
     {
-        Utilities.douwnLoadStatsFile() ;
+        Utilities.douwnLoadStatsFile();
     } // end of main method
-
 }// END of Aiborn class
